78af5ba12aa90be37737a4bf35c75abada25312f
Ari Trachtenberg <trachten@bu.edu>
2017-01-23 14:46:13 -0500

    another version

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int computeMe(int nn) {
     if (nn==0 || nn==1)
-        return 1;
+        return 2;
     else
-        return (3*computeMe(nn-1)+computeMe(nn-2))%1000;
+        return (2*computeMe(nn-1)+computeMe(nn-2))%1000;
 }
 
 int main() {
