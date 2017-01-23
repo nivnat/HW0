4533674d980daf0a0658adea595bf7779421b8a6
Ari Trachtenberg <trachten@bu.edu>
2017-01-23 14:36:34 -0500

    another version

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int computeMe(int nn) {
     if (nn==0 || nn==1)
         return 1;
     else
-        return (2*computeMe(nn-1)+computeMe(nn-2))%1000;
+        return (3*computeMe(nn-1)+computeMe(nn-2))%1000;
 }
 
 int main() {
